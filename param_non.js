e2bb2d51e089c9b4c78bafc454335bc9daabc79b
Werner von Bloh <bloh@pik-potsdam.de>
2019-03-21 15:52:33 +0100

    JSON files for run without nitrogen added

---
+++ param_non.js
@@ -0,0 +1,21 @@
+/**************************************************************************************/
+/**                                                                                \n**/
+/**                   p  a  r  a  m  _  n  o  n  .  j  s                           \n**/
+/**                                                                                \n**/
+/** Input parameter file for LPJmL C Version 5.1.001 without nitrogen              \n**/
+/**                                                                                \n**/
+/** (C) Potsdam Institute for Climate Impact Research (PIK), see COPYRIGHT file    \n**/
+/** authors, and contributors see AUTHORS file                                     \n**/
+/** This file is part of LPJmL and licensed under GNU AGPL Version 3               \n**/
+/** or later. See LICENSE file or go to http://www.gnu.org/licenses/               \n**/
+/** Contact: https://github.com/PIK-LPJmL/LPJmL                                    \n**/
+/**                                                                                \n**/
+/**************************************************************************************/
+
+#include "par/lpjparam_non.js"  /* LPJ parameter file */
+#include "par/soil.js"          /* Soil parameter file */
+#include "par/pft_non.js"       /* PFT parameter file without nitrogen */
+
+#include "par/manage_laimax_alphaa_fao.y.1999.2009_date.2017-10-19_lpj.rev.4176-4184M.js" /* Management parameter file */
+#include "par/manage_reg.js"    /* Management parameter file for regions*/
+#include "par/outputvars.js"
