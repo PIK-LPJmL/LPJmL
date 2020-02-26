c700198f889a51404ba4998b0ce7a307379c3a26
Werner von Bloh <bloh@pik-potsdam.de>
2020-02-26 09:37:23 +0100

    calc_nir() argument corrected in daily_biomass_grass.c

--- src/landuse/daily_biomass_grass.c
+++ src/landuse/daily_biomass_grass.c
@@ -230,7 +230,7 @@ Real daily_biomass_grass(Stand *stand, /**< stand pointer */
   }
 
   if(data->irrigation && stand->pftlist.n>0) /*second element to avoid irrigation on just harvested fields */
-    calc_nir(stand,&data->irrigation,gp_stand,wet,eeq);
+    calc_nir(stand,data,gp_stand,wet,eeq);
 
   forrootsoillayer(l)
   {
