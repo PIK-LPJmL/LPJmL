b2fded6a6abe74c63c5f3eb69242117160902a48
cmueller <cmueller@pik-potsdam.de>
2020-03-09 14:04:14 +0100

    bugfix lai_grass instead of lai_tree

--- src/grass/vmaxlimit_grass.c
+++ src/grass/vmaxlimit_grass.c
@@ -23,7 +23,7 @@ Real vmaxlimit_grass(const Pft *pft, /**< pointer to PFT */
   const Pftgrass *grass;
   Real vmax;
   grass=pft->data; 
-  vmax=((grass->ind.leaf.nitrogen-pft->par->ncleaf.low*grass->ind.leaf.carbon)*pft->nind)/exp(-param.k_temp*(temp-25))/f_lai(lai_tree(pft))/param.p/0.02314815*daylength;
+  vmax=((grass->ind.leaf.nitrogen-pft->par->ncleaf.low*grass->ind.leaf.carbon)*pft->nind)/exp(-param.k_temp*(temp-25))/f_lai(lai_grass(pft))/param.p/0.02314815*daylength;
   return max(vmax,epsilon);
 
   return (grass->ind.leaf.nitrogen-param.n0*0.001*(grass->ind.leaf.carbon))*pft->nind/exp(-param.k_temp*(temp-25))/f_lai(lai_grass(pft))/param.p/0.02314815*daylength;
