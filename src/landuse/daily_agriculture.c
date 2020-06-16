d6e819c0bb0398f9da852aed9c39572ba349c9fa
Christoph Mueller <cmueller@pik-potsdam.de>
2020-06-16 12:25:21 +0200

    killing crops if tmin < -5 and fphu between 0.2 and 0.95

--- src/landuse/daily_agriculture.c
+++ src/landuse/daily_agriculture.c
@@ -84,6 +84,21 @@ Real daily_agriculture(Stand *stand, /**< stand pointer */
 
   foreachpft(pft,p,&stand->pftlist)
   {
+#ifdef CROPSHEATFROST
+    /* kill crop at frost events */
+    if(climate->tmin<(-5))
+    {
+      crop=pft->data;
+      if(crop->fphu>0.2&&crop->fphu<0.95)
+      {
+        litter_update_crop(&pft->stand->soil.litter,pft,1.0);
+        delpft(&stand->pftlist,p);
+        stand->type=&kill_stand;
+        p--;
+        continue;
+      }
+    }
+#endif
     if(!config->with_nitrogen){
       pft->vscal=1;
     } else {
