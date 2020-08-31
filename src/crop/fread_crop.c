37aac97c546f7d6f18f68a748e5227cc0d3f605f
Jonas Jaegermeyr <jonasjae@pik-potsdam.de>
2020-08-31 23:35:46 +0200

    correct order of crop structure to read from restart

--- src/crop/fread_crop.c
+++ src/crop/fread_crop.c
@@ -53,9 +53,9 @@ Bool fread_crop(FILE *file, /**< file pointer */
   freadreal1(&crop->nfertilizer,swap,file);
   freadreal1(&crop->nmanure,swap,file);
   freadreal1(&crop->vscal_sum,swap,file);
-  freadreal1(&crop->supplysum,swap,file);
- #ifdef DOUBLE_HARVEST
   freadint1(&crop->frostkill,swap,file);
+ #ifdef DOUBLE_HARVEST
+  freadreal1(&crop->supplysum,swap,file);
   freadreal1(&crop->petsum,swap,file);
   freadreal1(&crop->evapsum,swap,file);
   freadreal1(&crop->transpsum,swap,file);
