9cd2f2f2bc9b5233cd9be18559497bdf02b01787
Christoph Mueller <cmueller@pik-potsdam.de>
2020-09-21 10:36:49 +0200

    correcting climate vs. climate_scenario loops

--- postprocessing/lpjml_ggcmi_output_processing_phase3b.R
+++ postprocessing/lpjml_ggcmi_output_processing_phase3b.R
@@ -17,17 +17,17 @@ registerDoSEQ() # tells foreach to use sequential mode
 
 NODATA <- 1e20
 ncell <- 67420
-sim.path <- "/p/projects/macmit/data/GGCMI/phase3/raw_output/phase3b"
+sim.path <- "/p/projects/macmit/data/GGCMI/phase3/raw_output2/phase3b"
 out.path <- "/p/projects/macmit/data/GGCMI/AgMIP.output/LPJmL/phase3b"
 
 # ----------------------------------------- #
 
-climate=c("gfdl-esm4","ipsl-cm6a-lr","mpi-esm1-2-hr","mri-esm2-0","ukesm1-0-ll")[5]
+climate=c("gfdl-esm4","ipsl-cm6a-lr","mpi-esm1-2-hr","mri-esm2-0","ukesm1-0-ll")#[5]
 
-climate_scenario=c("picontrol","historical","ssp126","ssp585")[4]
-start_years=c(1850,1850,2015,2015)[4]
-first_years=c(1850,1850,2015,2015)[4]
-end_years=c(2100,2014,2100,2100)[4]
+climate_scenarios=c("picontrol","historical","ssp126","ssp585")[2:4]
+start_years=c(1850,1850,2015,2015)[2:4]
+first_years=c(1850,1850,2015,2015)[2:4]
+end_years=c(2100,2014,2100,2100)[2:4]
 
 socioecon=c("histsoc","2015soc")[2]
 co2=c("default","2015co2","1850co2")[1]
@@ -143,48 +143,56 @@ hu_ref=t(hu_ref)
 close(ff)
 
 # ---------------------- #
+# climate scneario loop  #
+# ---------------------- #
+
+for(cs in 1:length(climate_scenarios))
+{
+  climate_scenario <- climate_scenarios[cs]
+  start.year=start_years[cs]
+  first.year=first_years[cs]
+  end.year=end_years[cs]
+  nyear=length(start.year:end.year)
+  
+# ---------------------- #
 # climate forcing loop
 # ---------------------- #
 
-for(cl in 1:length(climate)) {
-
+  for(cl in 1:length(climate)) {
+    
     print(" # ------------------------------------------- # ")
     print(paste(" # processing ", climate[cl]))
     print(" # ------------------------------------------- # ")
-
-    start.year=start_years[cl]
-    first.year=first_years[cl]
-    end.year=end_years[cl]
-    nyear=length(start.year:end.year)
-
+    
+    
     # input
     data.path=paste(sim.path,"/",climate_scenario,"/lpjml_phase3_",climate[cl],"_",socioecon,"_",co2,"_",start.year,"_",end.year,sep="")
-
+    
     # ------------------- #
     # irrig/rainfed loop
     # ------------------- #
-
+    
     for(ir in 1:2) { # 1: "noirr" 2: "firr"
-
+      
       if(!("pirnreq"%in%variables && ir==1)) {
-
+        
         parloop <- foreach(cr=c(1:length(crops)), .errorhandling='pass', .verbose=FALSE, .export='message') %dopar% {
-
+          
           print(paste("...",irrigs[ir],crops[cr]))
-
+          
           # ---------------------------- #
           # reading lpjml bin data
           # ---------------------------- #
-
+          
           syr <- read.LPJmL.array.int(paste(data.path,"/syear.bin",sep=""),first.year,start.year,end.year,30,if(ir>1) bands[cr]+15 else bands[cr],ncell)
           syr2 <- read.LPJmL.array.int(paste(data.path,"/syear2.bin",sep=""),first.year,start.year,end.year,30,if(ir>1) bands[cr]+15 else bands[cr],ncell)
-
+          
           var=syr
           var2=syr2
-
+          
           hu <- read.LPJmL.array(paste(data.path,"/husum.bin",sep=""),first.year,start.year,end.year,30,if(ir>1) bands[cr]+15 else bands[cr],ncell)
           hu2 <- read.LPJmL.array(paste(data.path,"/husum2.bin",sep=""),first.year,start.year,end.year,30,if(ir>1) bands[cr]+15 else bands[cr],ncell)
-
+          
           if("yield"%in%variables) {
             var <- read.LPJmL.array(paste(data.path,"/pft_harvestc.bin",sep=""),first.year,start.year,end.year,38,if(ir>1) bands[cr]+19 else bands[cr],ncell)/0.45/100
             var2 <- read.LPJmL.array(paste(data.path,"/pft_harvestc2.bin",sep=""),first.year,start.year,end.year,38,if(ir>1) bands[cr]+19 else bands[cr],ncell)/0.45/100
@@ -206,16 +214,16 @@ for(cl in 1:length(climate)) {
             var2 <- read.LPJmL.array.int(paste(data.path,"/sdate2.bin",sep=""),first.year,start.year,end.year,30,if(ir>1) bands[cr]+15 else bands[cr],ncell)
           }
           if("harvyear"%in%variables) {      
-
+            
             # calculate harvest year from syear, sdate, and growing period
             sd <- read.LPJmL.array.int(paste(data.path,"/sdate.bin",sep=""),first.year,start.year,end.year,30,if(ir>1) bands[cr]+15 else bands[cr],ncell)
             sd2 <- read.LPJmL.array.int(paste(data.path,"/sdate2.bin",sep=""),first.year,start.year,end.year,30,if(ir>1) bands[cr]+15 else bands[cr],ncell)
             gp <- read.LPJmL.array(paste(data.path,"/growing_period.bin",sep=""),first.year,start.year,end.year,34,if(ir>1) bands[cr]+17 else bands[cr],ncell)
             gp2 <- read.LPJmL.array(paste(data.path,"/growing_period2.bin",sep=""),first.year,start.year,end.year,34,if(ir>1) bands[cr]+17 else bands[cr],ncell)
-
+            
             hd=sd+gp
             hd2=sd2+gp2
-
+            
             var=syr
             var2=syr2
             for(y in 1:dim(var)[2]) {
@@ -226,50 +234,50 @@ for(cl in 1:length(climate)) {
             }
             rm(sd,sd2,gp,gp2,hd,hd2)
           }
-
+          
           if("soilmoist1m"%in%variables) smo <- read.LPJmL.array.monthly(paste(data.path,"/cft_mrootmoist.bin",sep=""),first.year,start.year,end.year,30,if(ir>1) bands[cr]+15 else bands[cr],ncell)
-
+          
           # ------------------------------------------ #
           # fixing double harvests ####
           # ------------------------------------------ #
-
+          
           testn <- which(rowSums(var2)>0)
           testn2 <- which(rowSums(syr2)>0) # just double checking
           if(length(testn)>0 & length(testn2)>0) if(length(which(testn%in%testn2))!=length(testn)) print(paste("WARNING! different set of double harvests in YLD2\n",paste(testn,collapse=" "),"\nand MDY\n",paste(testn2,collapse=" "),"\n"))
           rm(testn2)
-
+          
           # loop across cells with double harvest
           for(nfix in testn){
-
+            
             # extracting vectors of pixels with double harvests
             b.syr <- v.syr <- syr[nfix,]
             b.syr2 <- v.syr2 <- syr2[nfix,]
             b.hu <- v.hu <- hu[nfix,]
             b.hu2 <- v.hu2 <- hu2[nfix,]
-
+            
             b.var <- v.var <- var[nfix,]
             b.var2 <- v.var2 <- var2[nfix,]
-
+            
             index <- syr[nfix,]-start.year+1
             index2 <- syr2[nfix,]-start.year+1
             index[index<0] <- 0
             index2[index2<0] <- 0
             yearswithsowing <- array(0,length(index))
-
+            
             # fixing case by case
             # setting first harvests to right year
-
+            
             for(yf in 1:length(index)){
-
+              
               if(index[yf]!=0){
                 ind <- index[yf]
-
+                
                 v.var[ind] <- b.var[yf]
                 v.syr[ind] <- b.syr[yf]
                 v.hu[ind] <- b.hu[yf]
                 yearswithsowing[ind] <- 1
               } # index
-
+              
               if(index2[yf]!=0){
                 ind <- index2[yf]
                 v.var[ind] <- b.var2[yf]
@@ -281,7 +289,7 @@ for(cl in 1:length(climate)) {
                 yearswithsowing[ind] <- 1
               } # index2
             } # yf in index
-
+            
             # testing if all cases have been fixed #
             yfix2 <- which(v.var2>0)
             if(length(yfix2)>0) if(yfix2[1]!=length(v.var2)) print(paste("WARNING, still double harvests left:",paste(yfix2,collapse=" "),"\n"))
@@ -290,7 +298,7 @@ for(cl in 1:length(climate)) {
             if(length(yfix2)>1){
               print(paste(paste(nfix,collapse=" "),"\n",paste(syr[nfix,],collapse=" "),"\n",paste(syr2[nfix,],collapse=" "),"\n",paste(yearswithsowing,collapse=" "),"\n",paste(v.var,collapse=" "),"\n",paste(v.var2,collapse=" "),"\n"))
             }
-
+            
             # feeding back vectors with corrected double harvests
             v.var -> var[nfix,]
             v.var2 -> var2[nfix,]
@@ -298,86 +306,86 @@ for(cl in 1:length(climate)) {
             v.syr2 -> syr2[nfix,]
             v.hu -> hu[nfix,]
             v.hu2 -> hu2[nfix,]
-
+            
           } # nfix loop
-
+          
           # -------------------------------------------------- #
           # shift time series to have complete growing seasons #
           # -------------------------------------------------- #
-
+          
           # discarding first years' values if syear is before first year, but only of if all syear2 outputs are zero, i.e. the whole time series is shifted by one year
           discard <- which(syr[,1]<start.year)
-
+          
           print(paste("discarding first year values in",length(discard),"cases\n"))
-
+          
           var <- discard_first(var,discard)
           syr <- discard_first(syr,discard)
           hu <- discard_first(hu,discard)
           if("soilmoist1m"%in%variables) smo <- discard_first(smo,discard,TRUE)
-
+          
           # ----------------------------------------------------- #
           # dump harvest if less than 90% of heat units achieved  #
           # ----------------------------------------------------- #
-
+          
           if(hlimit==TRUE && ("yield"%in%variables || "matyday"%in%variables)) {
-
+            
             band_id=ifelse(ir>1,bands[cr]+15,bands[cr])
             for(y in 1:nyear) {
-
+              
               dump=ifelse(hu[,y]<0.90*hu_ref[,band_id],1,0)
-
-			        if("yield"%in%variables) var[which(dump==1),y]=0 # delete yields if less than 90% of husum is reached
+              
+              if("yield"%in%variables) var[which(dump==1),y]=0 # delete yields if less than 90% of husum is reached
               if("matyday"%in%variables) var[which(dump==1),y]=var[which(dump==1),y]*-1 # document the deletion of yield by setting matyday to negatve values
-
+              
             }
           }
-
+          
           # ------------------------------------------ #
           # write outputs to .nc4 file
           # ------------------------------------------ #
-
+          
           for(va in 1:length(variables)){
-
-  	        # 3b file name convention: lpjml_gfdl-esm4_w5e5_picontrol_histsoc_default_biom-wwh-firr_global_annual_1850_2100.nc 
-  	        # folder structure: AgMIP.output/<modelname>/phase3b/<climate_forcing>/<climate_scenario>/<crop>
-
-    		    # output dir
+            
+            # 3b file name convention: lpjml_gfdl-esm4_w5e5_picontrol_histsoc_default_biom-wwh-firr_global_annual_1850_2100.nc 
+            # folder structure: AgMIP.output/<modelname>/phase3b/<climate_forcing>/<climate_scenario>/<crop>
+            
+            # output dir
             outdir=paste(out.path,"/",climate[cl],"/",climate_scenario,"/",crops[cr],"/",sep="")
-
+            
             if(!file.exists(outdir)) dir.create(outdir,recursive=TRUE)
-
+            
             # output file
             timestep=ifelse(variables[va]=="soilmoist1m","monthly","annual")
             write_var=paste0(variables[va],"-",crops[cr],"-",irrigs[ir])
             fn <- paste(outdir,"lpjml_",climate[cl],"_w5e5_",climate_scenario,"_",socioecon,"_",co2,"_",write_var,"_global_",timestep,"_",start.year,"_",end.year,".nc",sep="")
             unlink(fn)
-
+            
             # NetCDF generation ####
             mv <- NODATA
-
+            
             # NC file dimensions
             dim_lon <- ncdim_def("lon","degrees_east",raster_lons)
             #change order of latitudes
             dim_lat <- ncdim_def("lat","degrees_north",rev(raster_lats))
             if(variables[va]=="soilmoist1m") {
-            	dim_time <- ncdim_def("time",paste("months since 1661-01-01, 00:00:00",sep=""),c(1:(nyear*12))+(start.year-1661)*12,calendar = "standard")
+              dim_time <- ncdim_def("time",paste("months since 1661-01-01, 00:00:00",sep=""),c(1:(nyear*12))+(start.year-1661)*12,calendar = "standard")
             } else {
               # start year of time dimension needs to be 1901 in 3a and 1661 in 3b, see https://www.isimip.org/protocol/preparing-simulation-files/
               dim_time <- ncdim_def("time",paste("growing seasons since 1661-01-01, 00:00:00",sep=""),c(start.year:end.year)-1660,calendar = "standard")
             }
-
+            
             # define variable
             ncv <- ncvar_def(write_var,units[va],list(dim_lon,dim_lat,dim_time),mv,longname=paste(irrigs[ir],cropf[cr],longnames[va]),compression=9)
-
+            
             # create files
             ncf <- nc_create(fn,list(ncv))
-
+            
             # commenting
             ncatt_put(ncf,varid=0,"title","LPJmL simulations for Ag-GRID GGCMI Phase 3 project")
             ncatt_put(ncf,varid=0,"comment1","Potsdam Institute for Climate Impact Research, Columbia University")
             ncatt_put(ncf,varid=0,"comment2","jonas.jaegermeyr@columbia.edu, cmueller@pik-potsdam.de, minoli@pik-potsdam.de")
             ncatt_put(ncf,varid=0,"comment3",fn)
-
+            
             # preparing data for NC files
             # having some 3D matrix: lon, lat, time
             if(variables[va]=="soilmoist1m") {
@@ -388,35 +396,35 @@ for(cl in 1:length(climate)) {
             # loop through pixels, fill matrix
             buf <- var
             if(variables[va]=="soilmoist1m") buf <- smo
-
+            
             lpjraster <- raster(ncols=720, nrows=360)
             lpj_raster_index <- cellFromXY(lpjraster,grid[,c(1,2)])
             mapo[lpj_raster_index,] <- buf
-
+            
             if(variables[va]=="soilmoist1m") {
-            	dim(mapo) <- c(nlon,nlat,nyear*12)
+              dim(mapo) <- c(nlon,nlat,nyear*12)
             } else {
-            	dim(mapo) <- c(nlon,nlat,nyear)
+              dim(mapo) <- c(nlon,nlat,nyear)
             }
-                  
+            
             ncvar_put(ncf,ncv,mapo,start=c(1,1,1),count=c(-1,-1,-1))
-
+            
             nc_close(ncf)
-
+            
           } # variables loop
-
+          
           # --------------------------------- #
-
+          
           if(length(warnings())>0) print(warnings())
-
+          
         } # overall crop loop
       } # if not pirnreq && ir==1
-  } # irrig loop
-
-  # --------------------------------- #
-
-} # climate
-
+    } # irrig loop
+    
+    # --------------------------------- #
+    
+  } # climate
+}
 # --------------------------------- #
 
 print("done")
