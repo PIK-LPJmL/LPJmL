e302dff3f7b1e82aefe55658ab0634cd2ab39e07
Werner von Bloh <bloh@pik-potsdam.de>
2021-02-11 11:18:24 +0100

    json files updated to new flags

--- lpjml_image_spinup.js
+++ lpjml_image_spinup.js
@@ -2,7 +2,7 @@
 /**                                                                                \n**/
 /**          l  p  j  m  l  _  i  m  a  g  e  _  s  p  i  n  u  p  .  j  s         \n**/
 /**                                                                                \n**/
-/** Configuration file for LPJmL C Version 5.1.002 spinup runs for IMAGE coupling  \n**/
+/** Configuration file for LPJmL C Version 5.3.001 spinup runs for IMAGE coupling  \n**/
 /**                                                                                \n**/
 /** Configuration file is divided into five sections:                              \n**/
 /**                                                                                \n**/
@@ -24,7 +24,6 @@
 
 #include "include/conf.h" /* include constant definitions */
 
-
 {   /* LPJmL configuration in JSON format */
 
 /*===================================================================*/
@@ -32,19 +31,19 @@
 /*===================================================================*/
 
   "sim_name" : "LPJmL Run", /* Simulation description */
-  "sim_id"   : "lpjml",       /* LPJML Simulation type with managed land use */
-  "version"  : "5.1",       /* LPJmL version expected */
+  "sim_id"   : "lpjml",     /* LPJML Simulation type with managed land use */
+  "version"  : "5.3",       /* LPJmL version expected */
   "random_prec" : true,     /* Random weather generator for precipitation enabled */
   "random_seed" : "random_seed", /* seed for random number generator */
   "radiation" : "cloudiness",  /* other options: CLOUDINESS, RADIATION, RADIATION_SWONLY, RADIATION_LWDOWN */
-  "fire" : "fire",        /* fire disturbance enabled, other options: NO_FIRE, FIRE, SPITFIRE, SPITFIRE_TMAX (for GLDAS input data) */
-  "fdi" : "nesterov_index",   /* different fire danger index formulations: WVPD_INDEX(needs GLDAS input data), NESTEROV_INDEX*/
+  "fire" : "fire",          /* fire disturbance enabled, other options: NO_FIRE, FIRE, SPITFIRE, SPITFIRE_TMAX (for GLDAS input data) */
+  "fdi" : "nesterov",       /* different fire danger index formulations: WVPD_INDEX(needs GLDAS input data), NESTEROV_INDEX*/
   "firewood" : false,
   "new_phenology": true,    /* GSI phenology enabled */
   "new_trf" : false,        /* new transpiration reduction function disabled */
   "river_routing" : true,
   "permafrost" : true,
-  "with_nitrogen" : "no_nitrogen", /* other options: NO_NITROGEN, LIM_NITROGEN, UNLIM_NITROGEN */
+  "with_nitrogen" : "no", /* other options: NO_NITROGEN, LIM_NITROGEN, UNLIM_NITROGEN */
   "store_climate" : true, /* store climate data in spin-up phase */
   "shuffle_climate" : false,
   "const_climate" : false,
@@ -53,36 +52,44 @@
   "new_seed" : false, /* read random seed from restart file */
   "equilsoil" : false,
   "population" : false,
-  "landuse" : "landuse", /* other options: NO_LANDUSE, LANDUSE, CONST_LANDUSE, ALL_CROPS */
+  "landuse" : "yes", /* other options: NO_LANDUSE, LANDUSE, CONST_LANDUSE, ALL_CROPS */
   "landuse_year_const" : 2000, /* set landuse year for CONST_LANDUSE case */
   "reservoir" : false,
-  "wateruse" : "no_wateruse",  /* other options: NO_WATERUSE, WATERUSE, ALL_WATERUSE */
+  "wateruse" : "no",  /* other options: NO_WATERUSE, WATERUSE, ALL_WATERUSE */
 #else
   "equilsoil" : true,
   "population" : false,
-  "landuse" : "no_landuse",
+  "landuse" : "no",
   "reservoir" : false,
-  "wateruse" : "no_wateruse",
+  "wateruse" : "no",
 #endif
-  "with_nitrogen" : "no_nitrogen",
   "prescribe_burntarea" : false,
   "prescribe_landcover" : "no_landcover", /* NO_LANDCOVER, LANDCOVERFPC, LANDCOVEREST */
   "sowing_date_option" : "no_fixed_sdate",   /* NO_FIXED_SDATE, FIXED_SDATE, PRESCRIBED_SDATE */
   "sdate_fixyear" : 1970,               /* year in which sowing dates shall be fixed */
   "intercrop" : true,                   /* intercrops on setaside */
-  "remove_residuals" : false,           /* remove residuals */
+  "residue_treatment" : "no_residue_remove",
   "residues_fire" : false,              /* fire in residuals */
-  "irrigation" : "lim_irrigation",        /* NO_IRRIGATION, LIM_IRRIGATION, POT_IRRIGATION, ALL_IRRIGATION */
-  "laimax_interpolate" : "laimax_cft",    /* laimax values from manage parameter file, */
+  "irrigation" : "lim",                 /* NO_IRRIGATION, LIM_IRRIGATION, POT_IRRIGATION, ALL_IRRIGATION */
+  "laimax_interpolate" : "laimax_cft",  /* laimax values from manage parameter file, */
                                         /* other options: LAIMAX_CFT, CONST_LAI_MAX, LAIMAX_INTERPOLATE */
   "rw_manage" : false,                  /* rain water management */
   "laimax" : 5,                         /* maximum LAI for CONST_LAI_MAX */
+  "tillage_type" : "all",
+  "till_startyear" : 1850,
+  "black_fallow" : false,
   "grassland_fixed_pft" : false,
   "grass_harvest_options" : false,
+  "mowing_days" : [152, 335],          /* Mowing days for grassland if grass harvest options are ser */
   "groundwater_irrigation": false,
   "aquifer_irrigation": false,
   "others_to_crop" : false,
   "istimber" : false,
+  "grassonly" : false,                  /* set all cropland including others to zero but keep managed grasslands */
+  "crop_phu_option" : "new",
+  "cropsheatfrost" : false,
+  "double_harvest" : false,
+
 
 /*===================================================================*/
 /*  II. Input parameter section                                      */
