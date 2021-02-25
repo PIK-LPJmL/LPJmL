0f975c2bd01d20155e30cd467a69a75001d94668
Werner von Bloh <bloh@pik-potsdam.de>
2021-02-25 13:32:35 +0100

    lakefrac setting to 1 fixed in newgrid.c

--- src/lpj/newgrid.c
+++ src/lpj/newgrid.c
@@ -547,6 +547,9 @@ static Cell *newgrid2(Config *config,          /* Pointer to LPJ configuration *
                   config->lakes_filename.name,i+config->startgrid);
           return NULL;
         }
+        if(grid[i].lakefrac>1-epsilon)
+          grid[i].lakefrac=1;
+
       }
     }
 #ifdef IMAGE
