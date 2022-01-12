ce96df570af245379b72687710c54f2421fb5235
Jannes <jannesbr@pik-potsdam.de>
2022-01-12 16:42:31 +0100

    documentation, add set_outputs method

--- python/lpjmlconfig.py
+++ python/lpjmlconfig.py
@@ -1,11 +1,12 @@
 import subprocess
 import json
+import warnings
 
 
 class LpjmlConfig:
     """This serves as an LPJmL config class that can be easily accessed,
-    converted to dict or written as a json file. It also provides methods to
-    set sockets for model coupling.
+    converted to a dictionary or written as a json file. It also provides 
+    methods to set sockets for model coupling.
 
     :param config_dict: takes a dictionary (ideally LPJmL config dictionary)
         and builds up a nested LpjmLConfig class with corresponding fields
@@ -22,18 +23,114 @@ class LpjmlConfig:
         return(list(self.__dict__.keys()))
 
     def get_outputs_avail(self):
-        """Get available output (outputvars) ids as list
+        """Get available output (outputvar) names (== output ids) as list
         """
-        pass
+        return [out.name for out in self.outputvar]
 
-    def set_outputs(self, path, outputs, temporal_resolution):
+    def get_outputs(self):
+        """Get defined output ids as list
+        """
+        return [out.id for out in self.output]
+
+    def set_outputs(self, output_path, outputs=[], file_format="raw",
+                    temporal_resolution="annual", append_output=False):
         """Set outputs to be written by LPJmL, define temporal resolution
+        :param output_path: define output_path the output is written to. If
+            `append_output == True` output_path is only altered for appended
+            `outputs`.
+        :type output_path: str
+        :param outputs: output ids of `outputs` to be written by LPJmL. Make sure
+            to check if required output is available via `get_outputs_avail`
+        :type outputs: list
+        :param file_format: file format for `outputs` (not to be used for
+            sockets!). "raw" (binary), "clm" (binary with header) and "cdf"
+            (NetCDF) are availble. Defaults to "raw".
+        :type file_format: str
+        :param temporal_resolution: list of temporal resolutions corresponding
+            to `outputs` or str to set the same resolution for all `outputs`.
+            Defaults to "annual" (for all `outputs`).
+        :type temporal_resolution: list/str
+        :param append_output: if True defined output entries are appended by
+            defined `outputs`. Please mind that the existing ones are not
+            altered.
+        :param append_output: bool
         """
-        pass
+        resolution_avail = ('annual', 'monthly', 'daily')
+        formats_avail = {'raw': 'bin', 'clm': 'clm', 'netcdf': 'cdf'}
+        if isinstance(temporal_resolution, list):
+            if any(
+                res not in resolution_avail for res in temporal_resolution
+            ):
+                raise ValueError("Temporal resolution not available for " +
+                                 "LPJmL. Choose from 'annual', 'monthly' " +
+                                 "and 'daily'.")
+            if len(outputs) != len(temporal_resolution):
+                raise ValueError("outputs and temporal_resolution have a " +
+                                 "different length. Please adjust.")
+        elif isinstance(temporal_resolution, str):
+            if temporal_resolution not in resolution_avail:
+                raise ValueError("Temporal resolution not available for " +
+                                 "LPJmL. Choose from 'annual', 'monthly' " +
+                                 "and 'daily'.")
+        else:
+            TypeError("Provide a list or a str to temporal_resolution")
+        # create dict of outputvar names with indexes for iteration
+        outputvar_names = {
+            ov.name: idx for idx, ov in enumerate(self.outputvar)
+        }
+        # extract dict of outputvar for manipulation
+        outputvars = self.to_dict()["outputvar"]
+        # empty list to collect if defined outputs are not in outputvar
+        not_found = []
+        if not append_output:
+            # empty output to be filled with defined entries (outputs)
+            self.output = []
+        # handle each defined output
+        for idx, out in enumerate(outputs):
+            # check temporal_resolution instance list/str
+            if isinstance(temporal_resolution, list):
+                temp_res = temporal_resolution[idx]
+            else:
+                temp_res = temporal_resolution
+            if out in outputvar_names:
+                new_out = {
+                    'id': outputvars[outputvar_names[out]]['name'],
+                    'file': {
+                        'fmt': file_format,
+                        'time_step': temp_res,
+                        'name': f"{output_path}/"
+                                f"{outputvars[outputvar_names[out]]['name']}"
+                                f".{formats_avail[file_format]}"
+                    }
+                }
+                self.output.append(self.__class__(new_out))
+            else:
+                # collect if defined outputs are not in outputvar
+                not_found.append(out)
+        if not_found:
+            # warning for defined outputs that are not listed in outputvar
+            warnings.warn("The following outputs do not exist in the" +
+                          f" current model version: {not_found}")
 
-    def set_restart(self, file):
+    def get_startfrom(self):
         """Set restart file from which LPJmL starts the transient run
         """
+        return self.restart_filename
+
+    def set_startfrom(self, path=None, file_name=None):
+        """Set restart file from which LPJmL starts the transient run
+        """
+        if path is not None:
+            file_name = self.restart_filename.split("/")
+            file_name.reverse()
+            self.restart_filename = f"{path}/{file_name[0]}"
+        elif file_name is not None:
+            file_check = file_name.split(".")
+            file_check.reverse()
+            if file_check[0] == 'lpj':
+                self.restart_filename = file_name
+        else:
+            raise ValueError('Please provide either a path or a file_name.')
 
     def set_sockets(self, inputs=[], outputs=[]):
         """Set sockets for inputs (corresponding key) and outputs (via id)
@@ -46,16 +143,16 @@ class LpjmlConfig:
         """
         for inp in inputs:
             sock_input = getattr(self, inp)
-            sock_input.__dict__ = {"fmt": "sock"}
+            sock_input.__dict__ = {'fmt': 'sock'}
         for out in self.output:
             if out.id in outputs:
-                out.file.__dict__ = {"fmt": "sock"}
+                out.file.__dict__ = {'fmt': 'sock'}
 
     def to_dict(self):
         """Convert class object to dictionary
         """
         def obj_to_dict(obj):
-            if not hasattr(obj, "__dict__"):
+            if not hasattr(obj, '__dict__'):
                 return obj
             result = {}
             for key, val in obj.__dict__.items():
@@ -127,3 +224,10 @@ def parse_config(path, js_filename="lpjml.js", spin_up=False,
     # convert to dict
     tmp_json = json.loads(tmp_json_str.stdout, object_hook=config)
     return(tmp_json)
+
+
+config = parse_config(
+    path="/p/projects/open/Jannes/copan_core/lpjml/LPJmL_internal"
+)
+
+config.set_outputs("./lol",["grid","npp","gpp"], temporal_resolution=["monthly","daily","lol"], append_output=False)
