57aefbfafa135119e99e04a590daf7836fed3dab
Werner von Bloh <bloh@pik-potsdam.de>
2022-05-19 12:54:05 +0200

    Fuel bulk density updated in harvest_crop.c

--- src/crop/harvest_crop.c
+++ src/crop/harvest_crop.c
@@ -50,6 +50,8 @@ void harvest_crop(Output *output,      /**< Output data */
     res_onfield=0.1;
   res_remove = (1-res_onfield);
   stand->soil.litter.item[pft->litter].ag.leaf.carbon += (crop->ind.leaf.carbon + crop->ind.pool.carbon)*res_onfield;
+  update_fbd_grass(&stand->soil.litter,pft->par->fuelbulkdensity,
+                   (crop->ind.leaf.carbon+crop->ind.so.carbon+crop->ind.pool.carbon)*res_onfield);
   getoutput(output,LITFALLC,config)+=(crop->ind.leaf.carbon + crop->ind.pool.carbon)*res_onfield*stand->frac;
   stand->soil.litter.item[pft->litter].ag.leaf.nitrogen += (crop->ind.leaf.nitrogen + crop->ind.pool.nitrogen)*res_onfield;
   getoutput(output,LITFALLN,config)+=(crop->ind.leaf.nitrogen + crop->ind.pool.nitrogen)*res_onfield*stand->frac;
