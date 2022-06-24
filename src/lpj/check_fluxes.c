ce854ab1243881a811cc6bf47ed458908920a6aa
Werner von Bloh <bloh@pik-potsdam.de>
2022-06-24 09:28:39 +0200

    Threshold set to 1.0 for carbon balance

--- src/lpj/check_fluxes.c
+++ src/lpj/check_fluxes.c
@@ -67,7 +67,7 @@ void check_fluxes(Cell *cell,          /**< cell pointer */
   else
     startyear=config->firstyear+1;
 
-  if(year>startyear && fabs(balance.carbon)>.2)
+  if(year>startyear && fabs(balance.carbon)>1.0)
   {
 #if defined IMAGE && defined COUPLED
     if(config->sim_id==LPJML_IMAGE)
