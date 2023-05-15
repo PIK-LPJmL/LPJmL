5326eb49a3f0ba96df13039d2519dec68f1d18c2
Werner von Bloh <bloh@pik-potsdam.de>
2023-05-15 15:54:32 +0200

    Missing update of HARVESTC and HARVESTN added

--- src/landuse/annual_biomass_tree.c
+++ src/landuse/annual_biomass_tree.c
@@ -149,6 +149,8 @@ Bool annual_biomass_tree(Stand *stand,         /**< Pointer to stand */
 #endif
         stand->cell->balance.biomass_yield.carbon+=yield.carbon*stand->frac;
         stand->cell->balance.biomass_yield.nitrogen+=yield.nitrogen*stand->frac;
+        getoutput(&stand->cell->output,HARVESTC,config)+=yield.carbon*stand->frac;
+        getoutput(&stand->cell->output,HARVESTN,config)+=yield.nitrogen*stand->frac;
         if(config->pft_output_scaled)
         {
           getoutputindex(&stand->cell->output,PFT_HARVESTC,rbtree(ncft)+biomass_tree->irrigation.irrigation*nirrig,config)+=yield.carbon*stand->frac;
