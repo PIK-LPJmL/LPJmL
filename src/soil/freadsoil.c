a0d529565fd3618d385736c29038f72e5352a3f5
Werner von Bloh <bloh@pik-potsdam.de>
2023-06-15 12:54:52 +0200

    snowcover not set to zero in freadsoil.c

--- src/soil/freadsoil.c
+++ src/soil/freadsoil.c
@@ -83,7 +83,7 @@ Bool freadsoil(FILE *file,             /**< pointer to binary file */
   soil->litter.decomC=0;
 #endif
   soil->YEDOMA=0;
-  soil->snowheight=soil->snowfraction=0;
+//  soil->snowheight=soil->snowfraction=0;
   freadreal1(&soil->meanw1, swap, file);
   freadreal1(&soil->wtable, swap, file);
   freadreal1(&soil->wa, swap, file);
