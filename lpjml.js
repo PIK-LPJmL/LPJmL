f96300d2b610aca30aba269700f4b3c7121079c7
Werner von Bloh <bloh@pik-potsdam.de>
2023-06-30 07:57:10 +0200

    CHANGELOG.md updated and version set to 5.6.15

--- lpjml.js
+++ lpjml.js
@@ -2,7 +2,7 @@
 /**                                                                                \n**/
 /**                   l  p  j  m  l  .  j  s                                       \n**/
 /**                                                                                \n**/
-/** Default configuration file for LPJmL C Version 5.6.14                          \n**/
+/** Default configuration file for LPJmL C Version 5.6.15                          \n**/
 /**                                                                                \n**/
 /** Configuration file is divided into five sections:                              \n**/
 /**                                                                                \n**/
