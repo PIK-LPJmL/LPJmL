488374d864623f3b8af6b8811085779c56f65500
Christoph Mueller <cmueller@pik-potsdam.de>
2024-07-18 13:54:07 +0200

    fixing floating point print statements

--- src/grass/fprintpar_grass.c
+++ src/grass/fprintpar_grass.c
@@ -25,8 +25,8 @@ void fprintpar_grass(FILE *file,const Pftpar *par,const Config *config)
                "C:N ratio:\t%g %g\n"
                "reprod cost:\t%g\n",
           1.0/pargrass->turnover.leaf,1.0/pargrass->turnover.root,
-          1/pargrass->nc_ratio.leaf,
-          1/pargrass->nc_ratio.root,
+          1.0/pargrass->nc_ratio.leaf,
+          1.0/pargrass->nc_ratio.root,
           pargrass->reprod_cost);
   if(config->with_nitrogen)
     fprintf(file,"rel. ratio:\t%g\n",pargrass->ratio);
