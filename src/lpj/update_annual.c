b5d5e36e44c801e9f371dd797df6be59d4b416b9
Werner von Bloh <bloh@pik-potsdam.de>
2024-09-27 12:04:03 +0200

    Setting vscal to NDAYYEAR removed in update_annual.c

--- src/lpj/update_annual.c
+++ src/lpj/update_annual.c
@@ -73,8 +73,6 @@ void update_annual(Cell *cell,          /**< Pointer to cell */
     stand->prescribe_landcover = config->prescribe_landcover;
 
     stand->soil.mean_maxthaw=(stand->soil.mean_maxthaw-stand->soil.mean_maxthaw/CLIMBUFSIZE)+stand->soil.maxthaw_depth/CLIMBUFSIZE;
-    foreachpft(pft,p,&stand->pftlist)
-      pft->vscal=NDAYYEAR;
     if(annual_stand(stand,npft,ncft,year,isdaily,intercrop,config))
     {
       /* stand has to be deleted */
