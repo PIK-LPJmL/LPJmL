3aee4c431291da76cbf06af6fca6bfe30f237936
Werner von Bloh <bloh@pik-potsdam.de>
2025-01-23 14:33:25 +0100

    ebullition function corrected

--- src/soil/ebullition.c
+++ src/soil/ebullition.c
@@ -35,48 +35,62 @@ Real ebullition(Soil *soil,   /**< pointer to soil data */
                 Real fpc_all  /**< plant cover  (0..1) */
               )
 {
-  Real C_thres, Q_ebull, ratio, Q_ebull_day, soil_moist[NSOILLAYER], V[NSOILLAYER], epsilon_CH4; //, epsilon_CH4_u;
+  Real C_thres, Q_ebull[BOTTOMLAYER], ratio, soil_moist[BOTTOMLAYER], V, epsilon_CH4[BOTTOMLAYER]; //, epsilon_CH4_u;
+  Real conc[BOTTOMLAYER];
+  Real start,end;
+  Real Q_ebull2;
   int l, i;
-  Q_ebull = Q_ebull_day = 0.0;
 #ifdef DEBUG
   printf("EBULL before:");
   printch4(soil->CH4);
 #endif
-  for (l = 0; l<NSOILLAYER; l++)
+  start=soilmethane(soil);
+  for (l = 0; l<BOTTOMLAYER; l++)
   {
     soil_moist[l] = getsoilmoist(soil,l);
-    V[l] = getV(soil,l);  /*soil air content (m3 air/m3 soil)*/
+    V = getV(soil,l);  /*soil air content (m3 air/m3 soil)*/
+    epsilon_CH4[l] = max(0.001, V + soil_moist[l]*soil->wsat[l]*BCH4);
+    conc[l]=soil->CH4[l]/soildepth[l]/epsilon_CH4[l]*1000.;
   }
   C_thres = CH4_min*(2 - fpc_all);
 
-  for (l = LASTLAYER - 1; l >= 0; l--)
+  for (i = 1; i <= 24; i++)
   {
-    epsilon_CH4 = max(0.001, V[l] + soil_moist[l] * soil->wsat[l]*BCH4);
-    ratio = min((layerbound[l] - soil->wtable) / soildepth[l], 1);
-    for (i = 1; i <= 24; i++)
+    for (l = 0; l<BOTTOMLAYER; l++)
+      Q_ebull[l]=0;
+    for (l = LASTLAYER - 1; l >= 0; l--)
     {
-      if ((soil->CH4[l]/soildepth[l]*1000/epsilon_CH4*ratio)>C_thres ) // && soil->CH4[l] / soildepth[l] * 1000 / epsilon_CH4 > soil->CH4[l - 1] / soildepth[l - 1] * 1000 / epsilon_CH4_u)
+  //    ratio = min((layerbound[l] - soil->wtable) / soildepth[l], 1);
+      ratio=1;
+      if(l<LASTLAYER-1)
+      {
+        if(soil_moist[l]>0.9)
+          Q_ebull[l]=Q_ebull[l+1];
+        else
+          soil->CH4[l]+=Q_ebull[l+1];
+      } 
+      if ((conc[l]*ratio)>C_thres ) // && soil->CH4[l] / soildepth[l] * 1000 / epsilon_CH4 > soil->CH4[l - 1] / soildepth[l - 1] * 1000 / epsilon_CH4_u)
       {
 /*
       if ((soil->CH4[l] / soildepth[l] * 1000 / epsilon_CH4*ratio)>C_thres && soil->wtable<layerbound[l] && soil->CH4[l] / soildepth[l] * 1000 / epsilon_CH4 > soil->CH4[l - 1] / soildepth[l - 1] * 1000 / epsilon_CH4_u)
       {
 */
-        Q_ebull=k_e*(soil->CH4[l]/soildepth[l]/epsilon_CH4*1000*ratio-C_thres)*soildepth[l]*epsilon_CH4*1e-3;
-        Q_ebull= max(0,min(soil->CH4[l],Q_ebull));
-        soil->CH4[l] -= Q_ebull;
-        if (l == 0)
-          Q_ebull_day += Q_ebull;
-        else
-          soil->CH4[l-1] += Q_ebull;
-        //printf("ebullition: %g layer: %d\n",Q_ebull,l);
+        Q_ebull2=k_e*(conc[l]*ratio-C_thres);
+        Q_ebull2= max(0,min(conc[l],Q_ebull2));
+        conc[l] -= Q_ebull2;
+        Q_ebull[l]+=Q_ebull2;
       }
       else
         break;
     }
   }
+  for (l = 0; l<BOTTOMLAYER; l++)
+    soil->CH4[l]=conc[l]*soildepth[l]*epsilon_CH4[l]*1e-3;
+  end=soilmethane(soil);
+
 #ifdef DEBUG
   printf("EBULL after:");
   printch4(soil->CH4);
 #endif
-  return Q_ebull_day;
+  return start-end;
 } /* of 'ebullition */
