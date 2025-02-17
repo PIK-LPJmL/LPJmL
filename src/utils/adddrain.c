9a9e36ad7076a3db83c4e53299a9e987293270f5
Werner von Bloh <bloh@pik-potsdam.de>
2025-02-17 08:50:50 +0100

    Type in error message fixed in adddrain.c

--- src/utils/adddrain.c
+++ src/utils/adddrain.c
@@ -139,7 +139,7 @@ int main(int argc,char **argv)
   }
   if(data_version>2 && header.datatype!=LPJ_INT)
   {
-    fprintf(stderr,"Invalid datatype %s in '%s', muste bei int.\n",typenames[header.datatype],argv[3]);
+    fprintf(stderr,"Invalid datatype %s in '%s', must be int.\n",typenames[header.datatype],argv[3]);
     return EXIT_FAILURE;
   }
   if(header.ncell!=n1)
