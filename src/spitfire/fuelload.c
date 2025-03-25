5ca5d9ced2fc7d37e2cf0730e81c42e711f720e1
Werner von Bloh <bloh@pik-potsdam.de>
2025-03-25 16:43:04 +0100

    Missing check for gsilivefuel added in fuelload.c

--- src/spitfire/fuelload.c
+++ src/spitfire/fuelload.c
@@ -133,23 +133,26 @@ void fuelload(const Stand *stand,  /**< pointer to stand */
   if(livegrass > 0)
   {
         /*compute live fuel moisture based on phen-LFMC empirical relation for each grass PFT, weighted by FPC */
-    fpc_grass_sum=0;
-    livefuel->M[0]=0;
-
-    foreachpft(pft,p,&stand->pftlist)
-    {
-      if(isgrass(pft))
-      {
-        grasspar=pft->par->data;
-        livefuel->M[0] += max(grasspar->lfmc_a, min(2.5, grasspar->lfmc_b + grasspar->lfmc_c*pft->phen)) * pft->fpc;
-        fpc_grass_sum += pft->fpc;
-      }
-    }
-    if(fpc_grass_sum>0)
-      livefuel->M[0] = livefuel->M[0]/fpc_grass_sum;
-   
+    if(config->gsilivefuel)
     /* use GSI based phenolgy */
-    livefuel->M[0]=max(0.3,min(2.5,4.4*stand->cell->gsi_cum-1.9));
+      livefuel->M[0]=max(0.3,min(2.5,4.4*stand->cell->gsi_cum-1.9));
+    else
+    {
+      fpc_grass_sum=0;
+      livefuel->M[0]=0;
+
+      foreachpft(pft,p,&stand->pftlist)
+      {
+        if(isgrass(pft))
+        {
+          grasspar=pft->par->data;
+          livefuel->M[0] += max(grasspar->lfmc_a, min(2.5, grasspar->lfmc_b + grasspar->lfmc_c*pft->phen)) * pft->fpc;
+          fpc_grass_sum += pft->fpc;
+        }
+      }
+      if(fpc_grass_sum>0)
+        livefuel->M[0] = livefuel->M[0]/fpc_grass_sum;
+    } 
 
     ratio_c3_livegrass = livefuel->pot_fc_lg_c3 / livegrass;
     ratio_c4_livegrass = livefuel->pot_fc_lg_c4 / livegrass;
