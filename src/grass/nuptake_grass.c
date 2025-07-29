b0d93b3257bb729a957d7386b5b54278107ffac5
Sibyll Schaphoff <sibylls@pik-potsdam.de>
2025-07-29 14:58:46 +0200

    correct fail call

--- src/grass/nuptake_grass.c
+++ src/grass/nuptake_grass.c
@@ -109,10 +109,10 @@ Real nuptake_grass(Pft *pft,             /**< pointer to PFT data */
         soil->NH4[l]-=NH4_up[l]*n_uptake/nupsum;
 #ifdef SAFE
         if (soil->NO3[l]<-epsilon)
-          fail(NEGATIVE_SOIL_NO3_ERR,TRUE,"Cell (%s) NO3=%g<0 in layer %d, NO3_up=%g, nuptake=%g, nupsum=%g",
+          fail(NEGATIVE_SOIL_NO3_ERR,TRUE,TRUE,"Cell (%s) NO3=%g<0 in layer %d, NO3_up=%g, nuptake=%g, nupsum=%g",
                 sprintcoord(line,&pft->stand->cell->coord),soil->NO3[l],l,NO3_up[l],n_uptake,nupsum);
         if (soil->NH4[l]<-epsilon)
-          fail(NEGATIVE_SOIL_NO3_ERR,TRUE,"Cell (%s) NH4=%g<0 in layer %d, NH4_up=%g, nuptake=%g, nupsum=%g",
+          fail(NEGATIVE_SOIL_NO3_ERR,TRUE,TRUE,"Cell (%s) NH4=%g<0 in layer %d, NH4_up=%g, nuptake=%g, nupsum=%g",
                 sprintcoord(line,&pft->stand->cell->coord),soil->NH4[l],l,NH4_up[l],n_uptake,nupsum);
 #endif
       }
