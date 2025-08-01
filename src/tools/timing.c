e93df6a8d12272395337b18933323f7349aa709f
Werner von Bloh <bloh@pik-potsdam.de>
2025-08-01 13:07:53 +0200

    Header added to timing.c

--- src/tools/timing.c
+++ src/tools/timing.c
@@ -1,2 +1,17 @@
+/**************************************************************************************/
+/**                                                                                \n**/
+/**                      t  i  m  i  n  g  .  h                                    \n**/
+/**                                                                                \n**/
+/**     Declaration of global variable timing                                      \n**/
+/**                                                                                \n**/
+/** (C) Potsdam Institute for Climate Impact Research (PIK), see COPYRIGHT file    \n**/
+/** authors, and contributors see AUTHORS file                                     \n**/
+/** This file is part of LPJmL and licensed under GNU AGPL Version 3               \n**/
+/** or later. See LICENSE file or go to http://www.gnu.org/licenses/               \n**/
+/** Contact: https://github.com/PIK-LPJmL/LPJmL                                    \n**/
+/**                                                                                \n**/
+/**************************************************************************************/
+
 #include "lpj.h"
+
 Timing timing={}; /* Global variable for timing */
