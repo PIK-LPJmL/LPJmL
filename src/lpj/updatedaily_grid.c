5247828bd14a8c1567c60363a2e278f7c248b50c
Stephen Wirth <wirth@pik-potsdam.de>
2026-01-24 12:17:29 +0100

    Fixed global water balance error and added daily balance check to CHECK_BALANCE

--- src/lpj/updatedaily_grid.c
+++ src/lpj/updatedaily_grid.c
@@ -13,6 +13,7 @@
 /**************************************************************************************/
 
 #include "lpj.h"
+#include "agriculture.h"  /* for Irrigation struct in debug output */
 
 void updatedaily_grid(Outputfile *output,  /**< Output file data */
                       Cell grid[],         /**< cell array */
@@ -52,6 +53,80 @@ void updatedaily_grid(Outputfile *output,  /**< Output file data */
       printfailerr(WRITE_OUTPUT_ERR,TRUE,"Cannot write output");
     exit(WRITE_OUTPUT_ERR);
   }
+
+#ifdef CHECK_BALANCE
+  /* Daily water balance check - catches balance errors as they occur */
+  if(config->river_routing)
+  {
+    int cell;
+    Real cum_prec = 0, cum_evap = 0, cum_transp = 0, cum_interc = 0;
+    Real cum_evap_lake = 0, cum_evap_res = 0, cum_discharge = 0;
+    Real cum_conv_loss = 0, cum_wateruse = 0, cum_excess = 0, cum_MT_water = 0;
+    Real total_surface = 0, total_soil = 0;
+    Real surface_storage_last_sum = 0, soil_storage_last_sum = 0;
+    Real total_area = 0;
+    Stand *stand;
+    int s;
+
+    for(cell = 0; cell < config->ngridcell; cell++)
+    {
+      if(!grid[cell].skip)
+      {
+        total_area += grid[cell].coord.area;
+        cum_prec += grid[cell].balance.aprec * grid[cell].coord.area;
+        cum_evap += grid[cell].balance.aevap * grid[cell].coord.area;
+        cum_transp += grid[cell].balance.atransp * grid[cell].coord.area;
+        cum_interc += grid[cell].balance.ainterc * grid[cell].coord.area;
+        cum_evap_lake += grid[cell].balance.aevap_lake * grid[cell].coord.area;
+        cum_evap_res += grid[cell].balance.aevap_res * grid[cell].coord.area;
+        cum_conv_loss += grid[cell].balance.aconv_loss_evap * grid[cell].coord.area;
+        cum_wateruse += grid[cell].balance.awateruse_hil;
+        cum_excess += grid[cell].balance.excess_water * grid[cell].coord.area;
+        cum_MT_water += grid[cell].balance.aMT_water * grid[cell].coord.area;
+        soil_storage_last_sum += grid[cell].balance.soil_storage_last;
+
+        /* Calculate current soil water including irrig_stor + irrig_amount for agriculture stands */
+        foreachstand(stand, s, grid[cell].standlist)
+        {
+          total_soil += soilwater(&stand->soil) * stand->frac * grid[cell].coord.area;
+          if(stand->data != NULL && isagriculture(stand))
+          {
+            Irrigation *irrig_data = stand->data;
+            total_soil += (irrig_data->irrig_stor + irrig_data->irrig_amount) * stand->frac * grid[cell].coord.area;
+          }
+        }
+      }
+      cum_discharge += grid[cell].balance.adischarge;
+
+      /* Surface storage */
+      total_surface += grid[cell].discharge.dmass_lake + grid[cell].discharge.dmass_river
+                     + grid[cell].discharge.dmass_gw + grid[cell].lateral_water * grid[cell].coord.area;
+      if(grid[cell].ml.dam)
+        total_surface += grid[cell].ml.resdata->dmass;
+      surface_storage_last_sum += grid[cell].balance.surface_storage_last;
+    }
+
+    /* Compute running balance */
+    Real cum_inputs = cum_prec + cum_MT_water;
+    Real cum_outputs = cum_evap + cum_transp + cum_interc + cum_evap_lake + cum_evap_res
+                     + cum_discharge + cum_conv_loss + cum_wateruse + cum_excess;
+    Real delta_surface = total_surface - surface_storage_last_sum;
+    Real delta_soil = total_soil - soil_storage_last_sum;
+    Real running_balance = cum_inputs - cum_outputs - delta_surface - delta_soil;
+    Real balance_per_area = (total_area > 0) ? running_balance / total_area : 0.0;
+
+    /* Fail if daily balance error exceeds threshold (same threshold as annual check) */
+    if(fabs(balance_per_area) > param.error_limit.w_global)
+    {
+      fail(GLOBAL_WATER_BALANCE_ERR, FAIL_ON_BALANCE, FALSE,
+           "Daily water balance error in year %d day %d: diff=%.5g (%.5g mm), "
+           "prec=%.5g, conv_loss=%.5g, delta_surface=%.5g, delta_soil=%.5g",
+           year, day, running_balance, balance_per_area,
+           cum_prec, cum_conv_loss, delta_surface, delta_soil);
+    }
+  }
+#endif
+
 #ifdef USE_TIMING
   timing_stop(UPDATEDAILY_GRID_FCN,tstart);
 #endif
