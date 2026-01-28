0387c436927df3cde1da72519d6bae4a68ac7907
Stephen Wirth <wirth@pik-potsdam.de>
2026-01-28 10:38:01 +0100

    Added cftfrac to outputs and fixed pft_harvest renaming.

--- benchmark/simulate_default.R
+++ benchmark/simulate_default.R
@@ -148,7 +148,7 @@ if (!is.null(runs_filter)) {
 }
 
 outputvars_benchmark <- c(
-  "grid", "terr_area", "land_area", "lake_area", "vegc", "soilc", "litc",
+  "grid", "terr_area", "land_area", "lake_area", "cftfrac", "vegc", "soilc", "litc",
   "vegn", "soiln", "soilnh4", "soilno3", "leaching", "n_immo",
   "n_mineralization", "n_volatilization", "n2_emis", "n2o_denit",
   "n2o_nit", "nuptake", "bnf", "firec", "flux_estabc", "gpp", "npp",
