1ec3311f38f90a572ec61e3e82891afa231481b1
Werner von Bloh <bloh@pik-potsdam.de>
2026-04-21 09:45:10 +0200

    Indentations updated in urban.c

--- src/landuse/urban.c
+++ src/landuse/urban.c
@@ -20,7 +20,7 @@
 #include "urban.h"
 
 Standtype urban_stand={URBAN,"urban",{0.0,0.0},0,new_agriculture,
-                             free_agriculture,fwrite_agriculture,
-                             fread_agriculture,fprint_agriculture,
-                             daily_natural,annual_agriculture,
-                             NULL};
+                       free_agriculture,fwrite_agriculture,
+                       fread_agriculture,fprint_agriculture,
+                       daily_natural,annual_agriculture,
+                       NULL};
