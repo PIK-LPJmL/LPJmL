f0e10485474dff19d427b92fd15a3760ab9bfeb6
Fabian Stenzel <fabian.stenzel@pik-potsdam.de>
2026-04-29 09:41:56 +0000

    Edit cmprestart.c

--- src/utils/cmprestart.c
+++ src/utils/cmprestart.c
@@ -4,7 +4,7 @@
 /**                                                                                \n**/
 /**     C implementation of LPJmL                                                  \n**/
 /**                                                                                \n**/
-/**     Utility compares twp restart files                                         \n**/
+/**     Utility compares two restart files                                         \n**/
 /**                                                                                \n**/
 /** (C) Potsdam Institute for Climate Impact Research (PIK), see COPYRIGHT file    \n**/
 /** authors, and contributors see AUTHORS file                                     \n**/
